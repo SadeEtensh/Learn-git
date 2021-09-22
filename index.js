fa3d476ec9859a5c9d764a4d9cb9a3a57e768f3d
SadeEtensh <eteneshsade550305@gmail.com>
2021-09-22 08:45:59 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-console.log("Hello git")
-console.log("Hello git")
-console.log("Hello git")
-console.log("Hello git")
-console.log("Hello git")
-console.log("Hello git")
-console.log("Hello git")
+const hello = document.getElementById("hello").innetHTML = 'HEllo JavaScript'
