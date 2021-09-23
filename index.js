fed4ea08d009fb0d0c845e61e0fd4ee8c13f2796
SadeEtensh <eteneshsade550305@gmail.com>
2021-09-23 07:51:49 +0100

    error fixed

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-const hello = document.getElementById("hello").innetHTML = 'Hello JavaScript'
+const hello = document.getElementById("hello").innetHTML = 'Hello JavaScript';
+const hello = document.getElementById("hello").innetHTML = 'Hello JavaScript';
