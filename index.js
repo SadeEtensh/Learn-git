d0ab4b67f575a8b5b93e7a361d161d4ec36b2d11
SadeEtensh <eteneshsade550305@gmail.com>
2021-09-30 14:38:37 +0100

    updated js file

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const hello = document.getElementById("hello").innetHTML = 'Hello JavaScript';
-const hello = document.getElementById("hello").innetHTML = 'Hello JavaScript';
+
 const hello = document.getElementById("hello").innetHTML = 'Hello JavaScript';
 const hello = document.getElementById("hello").innetHTML = 'Hello JavaScript';
